46c6278adfd759a91e93d2571ba21141e2447da0
Xianyang <chenxianyang.tom@gmail.com>
2017-10-18 15:39:56 +0800

    commit

--- kb_crawler.py
+++ kb_crawler.py
@@ -54,7 +54,7 @@ query_tmpl3 = Template('''
 PREFIX fb: <http://rdf.freebase.com/ns/>
 SELECT DISTINCT ?t
 WHERE {
-    fb:${e} fb:type.object.type ?t .
+    fb:${e} fb:common.topic.notable_types ?t .
 }
 ''')
 
@@ -65,7 +65,26 @@ def get_type(mid):
     if not results:
         logger.debug('Error in SPARQL query:\n%s', query)
     # print(results)
-    return sum(results, [])
+    return results[0][0]
+
+
+def get_candidates(seed, path):
+    lines = []
+    for i in xrange(len(path)):
+        if i == 0:
+            line = '%s %s ?x%d .' % (seed, path[i], i)
+        else:
+            line = '?x%d %s ?x%d .' % (i-1, path[i], i)
+        lines.append(line)
+    query = '''
+PREFIX fb: <http://rdf.freebase.com/ns/>
+SELECT DISTINCT ?x%d 
+WHERE {
+%s
+}
+''' % (len(path), '\n'.join(lines))
+    result = sparql_backend.query(query)
+    return sum(result, [])
 
 
 def retrieve(seeds):
@@ -75,10 +94,13 @@ def retrieve(seeds):
     for crawled_from_seed, seed in zip(crawled_one_hop, seeds):
         kg.add_node(seed)
         kg.nodes[seed].add_candidates([seed])
+        kg.nodes[seed].set_type(get_type(seed))
         for path in crawled_from_seed:
             x = kg.add_node()
             kg.add_edge(seed, x, path[0])
             kg.nodes[x].set_type(path[1])
+            cands = get_candidates(seed, path[0:1])
+            kg.nodes[x].add_candidates(cands)
     for crawled_from_seed, seed in zip(crawled_two_hop, seeds):
         for path in crawled_from_seed:
             predge = kg.find_edge(seed, None, path[0])[0]
@@ -87,6 +109,8 @@ def retrieve(seeds):
                 x = kg.add_node()
                 kg.add_edge(med, x, path[1])
                 kg.nodes[x].set_type(path[3])
+                cands = get_candidates(seed, path[0:2])
+                kg.nodes[x].add_candidates(cands)
             else:
                 x1 = kg.add_node()
                 x2 = kg.add_node()
@@ -102,8 +126,10 @@ def merge(kg):
     # add 'equal' between nodes with overlapping candidates, merge nodes with same candidates
     for n1, n2 in itertools.combinations(kg.nodes.keys(), 2):
         if kg.nodes[n1].type == kg.nodes[n2].type:
-            kg.add_edge(n1, n2, '*equal*')
-
+            # kg.add_edge(n1, n2, '*equal*')
+            if not kg.nodes[n1].candidates:
+                cands = get_candidates(kg, n1)
+                kg.nodes[n1].add_candidates(cands)
 
 def enhance(kg, literals):
     # add numerical relations
