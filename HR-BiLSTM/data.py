d421f1da6d33106736867beac68c54e95b18e79c
Xianyang <chenxianyang.tom@gmail.com>
2017-11-06 06:41:05 -0500

    commit

--- HR-BiLSTM/data.py
+++ HR-BiLSTM/data.py
@@ -5,7 +5,7 @@ def load_word_embedding():
     path = '/home/xianyang/bpqa/data/GoogleNews-vectors-negative300.bin'
     print('Loading word2vec data ...')
     model = gensim.models.Word2Vec.load_word2vec_format(path, binary=True)
-    sentence = ["london", "is", "the", "capital", "of", "great", "britain"]
+    sentence = ["london", "is", "the", "capital", "great", "britain"]
     vectors = [model[w] for w in sentence]
     # print(model.wv.most_similar(positive=['woman', 'king'], negative=['man']))
     print(vectors)
