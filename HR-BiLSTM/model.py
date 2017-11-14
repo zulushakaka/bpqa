9b5071086293f7685dabba1867ab365e32a3086d
Xianyang <chenxianyang.tom@gmail.com>
2017-11-14 18:51:15 -0500

    commit

--- HR-BiLSTM/model.py
+++ HR-BiLSTM/model.py
@@ -4,6 +4,9 @@ from data import load_word_embedding
 
 
 LSTM_HIDDEN_SIZE = 100
+MAX_QUESTION_LENGTH = 30
+MAX_RELATION_WORD_LEGNTH = 10
+MAX_RELATION_TYPE_LENGTH = 5
 
 
 class HRBiLSTM (object):
@@ -13,26 +16,43 @@ class HRBiLSTM (object):
 
     def build_model(self, embedding_matrix):
         # question representation
-        q_inputs = tf.placeholder(tf.int32, shape=(None))  # inputs are 1-dim integers
+
+        q_inputs = tf.placeholder(dtype=tf.int32, shape=[None, MAX_QUESTION_LENGTH])
+        # dimension: (batch_size, max_seq_len)
+        q_length = tf.placeholder(dtype=tf.int32, shape=[None])
+        # dimension: (batcj_size)
 
         embedding = tf.get_variable(name="embedding", shape=embedding_matrix.shape,
                                          initializer=tf.constant_initializer(embedding_matrix), trainable=False)
+        # dimension: (voc_size, embedding_dim)
         embedded = tf.nn.embedding_lookup(embedding, q_inputs)
+        # dimension: (batch_size, max_seq_len, embedding_dim)
 
         cell_q1_fw = tf.nn.rnn_cell.BasicLSTMCell(LSTM_HIDDEN_SIZE)
         cell_q1_bw = tf.nn.rnn_cell.BasicLSTMCell(LSTM_HIDDEN_SIZE)
-        q1_outputs, q1_states = tf.nn.bidirectional_dynamic_rnn(cell_q1_fw, cell_q1_bw, embedded, dtype=tf.float32)
+        q1_outputs, _ = tf.nn.bidirectional_dynamic_rnn\
+            (cell_fw=cell_q1_fw, cell_bw=cell_q1_bw, inputs=embedded, sequence_length=q_length, dtype=tf.float32)
+        q1_outputs = tf.concat(q1_outputs, 2)
+        # dimension: (batch_size, max_seq_len, cell_out_size * 2)
 
         cell_q2_fw = tf.nn.rnn_cell.BasicLSTMCell(LSTM_HIDDEN_SIZE)
         cell_q2_bw = tf.nn.rnn_cell.BasicLSTMCell(LSTM_HIDDEN_SIZE)
-        q2_outputs, q2_states = tf.nn.bidirectional_dynamic_rnn(cell_q2_fw, cell_q2_bw, tf.concat(q1_outputs, 2), dtype=tf.float32)
+        q2_outputs, _ = tf.nn.bidirectional_dynamic_rnn\
+            (cell_fw=cell_q2_fw, cell_bw=cell_q2_bw, inputs=q1_outputs, sequence_length=q_length, dtype=tf.float32)
+        q2_outputs = tf.concat(q2_outputs, 2)
+        # dimension: (batch_size, max_seq_len, cell_out_size * 2)
 
         q_outputs = tf.add(q1_outputs, q2_outputs)
-        q_pooling = tf.nn.max_pool(q_outputs)
+        # dimension: (batch_size, max_seq_len, cell_out_size * 2)
+
+        q_max_pool = tf.reduce_max(q_outputs, 1)
+        # dimension: (batch_size, cell_out_size* 2)
 
         # relation representation
-        r_inputs_word = tf.placeholder(tf.int32, shape=(None))
-        r_inputs_rels = tf.placeholder(tf.int32, shape=(None))
+
+        r_inputs_word = tf.placeholder(tf.int32, shape=[None, MAX_RELATION_WORD_LEGNTH])
+        # dimension: (batch_size, max_seq_len)
+        r_inputs_rels = tf.placeholder(tf.int32, shape=[None, MAX_RELATION_TYPE_LENGTH])
 
         r_embedding = load_word_embedding()
         r_embedding = tf.get_variable(name="r_embedding", shape=r_embedding.shape,
@@ -50,7 +70,7 @@ class HRBiLSTM (object):
         r_outputs = tf.concat(tf.concat(r_word_outputs, 2), tf.concat(r_rels_outputs, 2))
         r_pooling = tf.nn.max_pool(r_outputs)
 
-        score = tf.losses.cosine_distance(q_pooling, r_pooling)
+        score = tf.losses.cosine_distance(q_max_pool, r_pooling)
 
     def train(self):
         pass
